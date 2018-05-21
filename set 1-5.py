31f1bde2d410ab006ce950e801444fb17fc72a86
SakthiSubramaniyam <36299117+SakthiSubramaniyam@users.noreply.github.com>
2018-05-21 12:25:31 +0530

    Update set 1-5.py

--- set 1-5.py
+++ set 1-5.py
@@ -1,12 +1,12 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-num3 = float(input("Enter third number: "))
+num1 = float(input("Enter 1st number: "))
+num2 = float(input("Enter 2nd number: "))
+num3 = float(input("Enter 3rd number: "))
  
 if (num1 > num2) and (num1 > num3):
-   largest = num1
+   biggest = num1
 elif (num2 > num1) and (num2 > num3):
-   largest = num2
+   biggest = num2
 else:
-   largest = num3
+   biggest = num3
  
 print("The largest number is",largest)
